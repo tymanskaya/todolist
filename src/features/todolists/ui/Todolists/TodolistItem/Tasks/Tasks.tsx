00cbc42fe58442cd38617acaf6d02feff584b78a
ekaterinapupeka <etymanskaya@bk.ru>
2026-03-06 21:28:40 +0300

    pagination/PAGE_SIZE

--- src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
@@ -8,6 +8,7 @@ import { useState } from "react"
 import {
   TasksPagination
 } from "@/features/todolists/ui/Todolists/TodolistItem/Tasks/TasksPagination/TasksPagination.tsx"
+import { PAGE_SIZE } from "@/common/constants"
 
 type Props = {
   todolist: DomainTodolist
@@ -40,7 +41,18 @@ export const Tasks = ({ todolist }: Props) => {
           <List>{filteredTasks?.map((task) =>
             <TaskItem key={task.id} task={task} todolist={todolist} />)}
           </List>
-          <TasksPagination totalCount={data?.totalCount || 0} page={page} setPage={setPage} />
+          {(data?.totalCount ?? 0) > PAGE_SIZE && (
+            //Условие: рендерим, если общее количество больше размера страницы
+            //Типизация: data?.totalCount может быть undefined, пока идет первый
+            // запрос. Оператор ?? (nullish coalescing) подставляет 0, делая сравнение с числом безопасным.
+            //Оператор Nullish Coalescing (??) — это идеальный инструмент в данном случае.
+            // Он возвращает правое значение (0) только в том случае, если левое значение равно null или undefined.
+            <TasksPagination
+              totalCount={data?.totalCount || 0}
+              page={page}
+              setPage={setPage}
+            />
+          )}
         </>
 
       )}
